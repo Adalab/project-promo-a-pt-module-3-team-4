f91b1bb25386d4cccad778e9da629ecb78da52e6
Larafdezmorena <larafdezmorena@gmail.com>
2024-01-25 21:33:17 +0100

    add project template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,135 +1,100 @@
 import '../scss/App.scss'
+import adalabImage from '../images/adalab.png'
+//import avatarImage from '../images/avatar.webp'
+//import ebookImage from '../images/ebook-example.jpeg'
+import laptopImage from '../images/laptop-code-solid.svg'
+//import githubIcon from '../images/icons/github.svg'
+//import globeIcon from '../images/icons/globe-solid.svg'
 
 function App() {
   return (
     <div>
-       <div className="container">
-      <header className="header">
-        <p className="text">Proyectos Molones</p>
-      </header>
-      <main className="main">
-        <section className="preview">
-          <img className="image" src="./images/cover.jpeg" alt="" />
+    <div className="container">
 
-          <section className="autor">
-            <section className="info-project">
-              <p className="subtitle">Personal Project Card</p>
-              <hr className="line" />
+    <header className="header">
+      <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
+        <img className="header__companyLogo" src={laptopImage} alt="Logo proyectos molones"/>
+        <h1 className="header__title">Proyectos molones</h1>
+      </a>
+      <img className="logoSponsor" src={adalabImage} alt="Logo Adalab"/>
+    </header>
+    
+    <main className="main">
+      <section className="hero">
+        <h2 className="title">Proyectos molones</h2>
+        <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
+        <a className="button--link" href="./">Ver proyectos</a>
+      </section>
 
-              <h2 className="title">Elegant Workspace</h2>
-              <p className="slogan">Diseños Exclusivos</p>
-              <p className="desc">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Libero, delectus? Voluptates at hic aliquam porro ad suscipit
-                harum laboriosam saepe earum doloribus aperiam, ullam culpa
-                accusantium placeat odit corrupti ipsum!
-              </p>
-              <section className="technologies">
-                <p className="text">React JS, MongoDB</p>
-              </section>
-            </section>
+      <section className="preview">
+        <div className="projectImage"></div>
+        <article className="card">
+          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
 
-            <section className="info-autor">
-              <img className="image" src="./images/user.jpeg" alt="" />
-              <p className="job">Full Stack Developer</p>
-              <p className="name">Emmelie Björklund</p>
-            </section>
-          </section>
-        </section>
+          <div className="card__author">
+            <div className="card__authorPhoto"></div>
+            <p className="card__job">
+              Full stack Developer
+            </p>
+            <h3 className="card__name">Emmelie Bjôrklund</h3>
+          </div>
+      
+          <div className="card__project">            
+            <h3 className="card__name">Elegant Workspace</h3>
+            <p className="card__slogan">Diseños Exclusivos</p>
+            <h3 className="card__descriptionTitle">Product description</h3>
+            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
 
-        <section className="form">
-          <h2 className="title">Información</h2>
+            <div className="card__technicalInfo">
+              <p className="card__technologies">React JS - HTML - CSS</p>
+          
+              <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
+                Web link
+              </a>
+              <a className="icon icon__github" href="#" title="Haz click para ver el código del proyecto">
+                GitHub link
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+      <form className="addForm">
+        <h2 className="title">Información</h2>
+        <fieldset className="addForm__group">
+          <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
+          <input className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto"/>
+          <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan"/>
+          <div className="addForm__2col">
+            <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio"/>
+            <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo"/>
+          </div>         
+          <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías"/>
+          <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Descripción" rows="5"></textarea>
+        </fieldset>
+    
+        <fieldset className="addForm__group">
+          <legend className="addForm__title">Cuéntanos sobre la autora</legend>
+          <input className="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre"/>
+          <input className="addForm__input" type="text" name="job" id="job" placeholder="Trabajo"/>
+        </fieldset>
+    
+        <fieldset className="addForm__group--upload">
+          <label htmlFor="image" className="button">Subir foto del proyecto</label>
+          <input className="addForm__hidden" type="file" name="image" id="image"/>
+          <label htmlFor="photo" className="button">Subir foto de la autora</label>
+          <input className="addForm__hidden" type="file" name="photo" id="photo"/>
+          <button className="button--large">Guardar proyecto</button>
+        </fieldset>
+        
+      </form>
+    </main>
+  </div>
+   
 
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre el proyecto</p>
-            <hr className="line" />
-          </section>
-
-          <fieldset className="project">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre del proyecto"
-              name="name"
-              id="name"
-            />
-            <input
-              className="input"
-              type="text"
-              name="slogan"
-              id="slogan"
-              placeholder="Slogan"
-            />
-            <input
-              className="input"
-              type="text"
-              name="repo"
-              id="repo"
-              placeholder="Repo"
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Demo"
-              name="demo"
-              id="demo"
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Tecnologías"
-              name="technologies"
-              id="technologies"
-            />
-            <textarea
-              className="textarea"
-              type="text"
-              placeholder="Descripción"
-              name="desc"
-              id="desc"
-            ></textarea>
-          </fieldset>
-
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre la autora</p>
-            <hr className="line" />
-          </section>
-
-          <fieldset className="autor">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre"
-              name="autor"
-              id="autor"
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Trabajo"
-              name="job"
-              id="job"
-            />
-          </fieldset>
-
-          <section className="buttons-img">
-            <button className="btn">Subir foto de proyecto</button>
-            <button className="btn">Subir foto de autora</button>
-          </section>
-          <section className="buttons-img">
-            <button className="btn-large" onClick="{handleClickCreateCard}">
-              Crear Tarjeta
-            </button>
-          </section>
-
-          <section className="card">
-            <span className=""> La tarjeta ha sido creada: </span>
-            <a href="" className="" target="_blank" rel="noreferrer"> </a>
-          </section>
-        </section>
-      </main>
-    </div>
-    </div>
+    <footer className="footer">
+      <img className="logoSponsor" src={adalabImage} alt="Logo Adalab"/>
+    </footer>
+    </div> 
   )
 }
 
