4cadb6b9df3008dacf87bc216c9fd6c8536bdb7f
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-29 16:20:41 +0100

    update card

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,19 +1,11 @@
-import InputText from './InputText'
+import InputText from "./InputText";
 
 function Author() {
   return (
     <fieldset className="addForm__group">
       <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-      <InputText
-        name="autor"
-        id="autor"
-        placeholder="Nombre"
-        />
-      <InputText
-        name="job"
-        id="job"
-        placeholder="Trabajo"
-        />
+      <InputText name="autor" id="autor" placeholder="Nombre" />
+      <InputText name="job" id="job" placeholder="Trabajo" />
     </fieldset>
   );
 }
